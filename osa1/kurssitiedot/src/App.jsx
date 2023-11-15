a85a8584d9af63fb79dd86f411c5d349e04e1a63
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-15 09:29:23 +0200

    exerc 1.1-1.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,6 +1,36 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+const Header = (props) => {
+  return(
+      <div>
+          <h1>{props.course.name}</h1>
+      </div>
+  )
+}
+
+const Content = (props) => {
+  return(
+      <div>
+          <Part part={props.parts[0]} />
+          <Part part={props.parts[1]} />
+          <Part part={props.parts[2]} />
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+      <div>
+          <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+      <div>
+          <p>{props.part.name} {props.part.exercises}</p>
+      </div>
+  )
+}
 
 const App = () => {
   const course = {
