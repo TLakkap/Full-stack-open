775fa68fe681f10aa235e03b32570c5af81429e2
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-16 13:20:47 +0200

    exerc 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,26 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchParam, setSearchParam] = useState('')
-  const [filteredPersons, setFilteredPersons] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const search = (param) => {
     if(param === ''){
-      setFilteredPersons(persons);
+      setPersons(persons);
     }else{
       let foundPersons = persons.filter(el => el.name.toLowerCase().indexOf(param.toLowerCase()) !== -1)
-      setFilteredPersons(foundPersons);
+      setPersons(foundPersons);
     }
   }
 
@@ -65,7 +68,7 @@ const App = () => {
       <h2>Add a new</h2>
       <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons filteredPersons={filteredPersons} />
+      <Persons persons={persons} />
     </div>
   )
 }
--- osa2/puhelinluettelo/src/components/Persons.jsx
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -1,7 +1,7 @@
 const Persons = (props) => {
     return(
         <div>
-            {props.filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+            {props.persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
         </div>
     )
 }
