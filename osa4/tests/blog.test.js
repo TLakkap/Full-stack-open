e6071053e28da6c00bfd0d85c4bd2220c74b5b09
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-20 09:39:43 +0200

    exerc 4.6

--- osa4/tests/blog.test.js
+++ osa4/tests/blog.test.js
@@ -66,16 +66,27 @@ describe('total likes', () => {
 })
 
 describe('favorite blog', () => {
-    test('when list has multiple blogs', () => {
-      const result = listHelper.favoriteBlog(blogs)
-      expect(result).toEqual(
-        {
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12,
-            __v: 0
-        })
-    })
+  test('when list has multiple blogs', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(
+      {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+      })
   })
+})
+
+describe('author with', () => {
+  test('most blogs', () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual(
+      {
+        author: "Robert C. Martin",
+        blogs: 3
+      })
+  })
+})
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
@@ -14,9 +16,25 @@ const favoriteBlog = (blogs) => {
     const maxLikes = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current);
     return maxLikes
 }
+
+const mostBlogs = (blogs) => {
+    const groupedByAuthor = _.groupBy(blogs, 'author')
+    
+    const authorWithMostBlogs = _.maxBy(Object.keys(groupedByAuthor), author => {
+        return groupedByAuthor[author].length;
+    });
+      
+    const mostBlogsCount = groupedByAuthor[authorWithMostBlogs].length
+      
+    return {
+    author: authorWithMostBlogs,
+    blogs: mostBlogsCount,
+    }
+}
   
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
