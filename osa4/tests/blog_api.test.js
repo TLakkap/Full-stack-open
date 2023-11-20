d04961c61fd3c89d9078433f1c925d743de7c4c8
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-20 22:04:18 +0200

    exerc 4.11-4.12

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -61,6 +61,43 @@ test('a valid blog can be added', async () => {
     )
 })
 
+test(`adding a blog with likes undefined, likes are saved as 0`, async () => {
+    const newBlog = {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html"
+    }
+
+    const response = await api.post('/api/blogs').send(newBlog)
+    expect(response.body.likes).toBe(0)
+})
+
+test('a blog without title can not be added', async () => {
+    const newBlog = {
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(500)
+})
+
+test('a blog without url can not be added', async () => {
+    const newBlog = {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(500)
+})
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
