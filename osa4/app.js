c001f023ae7bf579a7713ff4cab54a8606517984
TLakkap <tiina.lakkapaa@gmail.com>
2023-11-22 10:09:01 +0200

    exerc 4.15

--- osa4/app.js
+++ osa4/app.js
@@ -4,6 +4,8 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -14,6 +16,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
