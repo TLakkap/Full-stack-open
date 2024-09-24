28e691c2c35fe1deeeb472a46252ae3ecefe7c76
TLakkap <tiina.lakkapaa@gmail.com>
2024-09-24 15:21:17 +0300

    exerc 6.10

--- osa6/redux-anecdotes/src/reducers/filterReducer.js
+++ osa6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,25 @@
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter,
-    }
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-const filterReducer = (state = '', action) => {
-    switch (action.type) {
-        case 'SET_FILTER':
-            return action.payload
-        default:
-            return state
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        filterReducer(state = '', action) {
+            switch (action.type) {
+                case 'SET_FILTER':
+                    return action.payload
+                default:
+                    return state
+            }
+        },
+        filterChange(filter) {
+            return {
+                type: 'SET_FILTER',
+                payload: filter,
+            }
+        }
     }
-}
+})
 
-export default filterReducer
+export const { filterReducer, filterChange } = filterSlice.actions
+export default filterSlice.reducer
